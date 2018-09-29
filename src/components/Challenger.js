9e70d5b2a4a5f169a35a39f8bdfafd6dd09a69e8
Allan Wilson <akwilson76@googlemail.com>
2018-09-29 15:39:53 +0100

    Disable challenge button with invalid challenger input

--- src/components/Challenger.js
+++ src/components/Challenger.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
+import Web3 from "web3"
 import { opponentChange, newGame } from "../actions"
 
 import "./Challenger.css"
@@ -9,6 +10,11 @@ const mapStateToProps = state => {
 }
 
 class Challenger extends Component {
+    isChallengable() {
+        const opponent = this.props.accounts.opponent
+        return opponent && opponent !== this.props.accounts.player && Web3.utils.isAddress(opponent)
+    }
+
     doChallenge() {
         this.props.dispatch(newGame(this.props.accounts))
     }
@@ -21,19 +27,19 @@ class Challenger extends Component {
         const accounts = this.props.accounts
 
         return (
-			<fieldset>
-            	<legend>New Game</legend>
-            	<div>
-                	<label className="block">Your Account</label>
-                	<span id="userAccount">{accounts.player}</span>
-            	</div>
-            	<div className="row2">
-                	<label className="block">Opponent Account</label>
-                	<input id="oppAccount" type="text" onChange={e => this.opponentChange(e.target.value)}
+            <fieldset>
+                <legend>New Game</legend>
+                <div>
+                    <label className="block">Your Account</label>
+                    <span id="userAccount">{accounts.player}</span>
+                </div>
+                <div className="row2">
+                    <label className="block">Opponent Account</label>
+                    <input id="oppAccount" type="text" onChange={e => this.opponentChange(e.target.value)}
                            size="45" value={accounts.opponent}></input>
-                	<button id="btnNew" className="bc2" onClick={e => this.doChallenge()}>Challenge</button>
-            	</div>
-        	</fieldset>
+                    <button id="btnNew" className="bc2" onClick={e => this.doChallenge()} disabled={!this.isChallengable()}>Challenge</button>
+                </div>
+            </fieldset>
         )
     }
 }
