1be87ac023b5eb4e8279aaae24d5300a84167990
Allan Wilson <akwilson76@googlemail.com>
2018-09-30 18:33:12 +0100

    Add connect4 icon

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react"
+import logo from "../connect4.svg"
 
 class HeaderBar extends Component {
     render() {
         return (
-            <h3>Crypto Connect 4</h3>
+            <div className="my-2 col-3 offset-4" >
+                <h3>
+                    <img src={logo} width="20" height="20"/>
+                    <span className="ml-2">Crypto Connect 4</span>
+                </h3>
+            </div>
         )
     }
 }
