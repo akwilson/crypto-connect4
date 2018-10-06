f39650289c450d7740bde35a7b77b7026e7ff209
Allan Wilson <akwilson76@googlemail.com>
2018-10-06 19:46:22 +0100

    Remove commented out crap

--- src/components/BoardTabs.js
+++ src/components/BoardTabs.js
@@ -41,22 +41,18 @@ class BoardTabs extends Component {
         }
 
         const gameTabs = _.map(games, (v, k) => {
-            //const refClasses = `nav-link ${selectedGame === k ? "active" : ""}`
             const refClasses = `no-trans btn btn-link nav-link ${selectedGame === k ? "active" : ""}`
             return (
                 <li key={k} className="nav-item">
-                    {/*<a className={refClasses} href="#" onClick={e => this.switch(k)}>{v.title}</a>*/}
                     <button className={refClasses} onClick={e => this.switch(k)}>{v.title}</button>
                 </li>
             )
         })
 
         if (pendingStart) {
-            //const refClasses = `nav-link ${selectedGame === -1 ? "active" : ""}`
             const refClasses = `no-trans btn btn-link nav-link ${selectedGame === -1 ? "active" : ""}`
             gameTabs.push(
                 <li key={gameTabs.length} className="nav-item">
-                    {/*<a className={refClasses} href="#" onClick={e => this.switch(-1)}>Pending...</a>*/}
                     <button className={refClasses} onClick={e => this.switch(-1)}>Pending...</button>
                 </li>
             )
